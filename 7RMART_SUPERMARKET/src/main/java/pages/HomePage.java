22df91adc22aac77bffe6f0d2fa346f7b467a0fe
SWAPNA <SWAPNA@DESKTOP-2TTEA4F>
2024-12-21 12:17:31 +0530

    second

--- 7RMART_SUPERMARKET/src/main/java/pages/HomePage.java
+++ 7RMART_SUPERMARKET/src/main/java/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import utilities.PageUtility;
+//import utilities.PageUtility;
 
 public class HomePage {
 	WebDriver driver;
